c4d3d93223927c425ce44975f3d1f80d053eed87
Naoufal <naoufal.sr10@gmail.com>
2025-04-21 18:27:47 +0200

    test nodejs 2

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require('express');
+var admin = require('firebase-admin');
+
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  let posts = [
-    {
-      caption: 'Post 1',
-      location: 'This is the content of post 1',
-    },
-    {
-      caption: 'Post 2',
-      location: 'This is the content of post 2',
-    },
-    {
-      caption: 'Post 3',
-      location: 'This is the content of post 3',
-    },
-  ];
-  res.json(posts);
+app.get('/posts', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  let posts = [];
+  db.collection('posts')
+    .orderBy('date', 'desc')
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        posts.push(doc.data());
+      });
+      res.send(posts);
+    })
+    .catch((error) => {
+      console.log('Error getting documents: ', error);
+    });
 });
 
 app.listen(process.env.PORT || 3000);
