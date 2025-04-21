8ea7f35dbd02336dae4f5dba50e6d9b53ade76b5
Naoufal <naoufal.sr10@gmail.com>
2025-04-21 18:55:55 +0200

    test nodejs 3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var admin = require('firebase-admin');
 
-const serviceAccount = require('./serviceAccountKey.json');
+//const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
